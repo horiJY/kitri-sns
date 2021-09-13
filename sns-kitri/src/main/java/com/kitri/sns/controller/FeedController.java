84e266221af6e88376d8855453db01e1349b96fb
DESKTOP-62O42Q2\JY-note <hori_q@naver.com>
2021-09-14 01:19:29 +0900

    feed service logic 수정

--- sns-kitri/src/main/java/com/kitri/sns/controller/FeedController.java
+++ sns-kitri/src/main/java/com/kitri/sns/controller/FeedController.java
@@ -1,26 +1,32 @@
 package com.kitri.sns.controller;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FeedController {
 
-//	@RequestMapping("")
-//	public String feedboard(Model model, Map<String,String> map) {
-//
-//		return "feedboard";
-//	}
-	
-	@GetMapping("")
-	public String feedboard() {
-		//session.id 없으면 login페이지로 이동
-		
-		//로그인 후 피드 가져오기
-		return "feedboard";
-	}
+  // @RequestMapping("")
+  // public String feedboard(Model model, Map<String,String> map) {
+  //
+  // return "feedboard";
+  // }
+
+  @GetMapping("")
+  public String feedboard() {
+    // session.id 없으면 login페이지로 이동
+
+    // 로그인 후 피드 가져오기
+    // 1 팔로잉_num>0
+    // 2 following 한 사람들의 3일 내의 feed를 가져옴
+    // 3 2의 feed를 다 본 경우 내가 팔로잉하는 사람들이 가장 많이 팔로잉한 사람의 feed를 가져옴
+
+
+    // 1-1 팔로잉_num=0
+    // 2-1 최근 3일간 등록된 feed 중에 like수가 많은 수대로 가져옴
+    // 3-1 2-1의 피드를 다 본 경우
+
+
+    return "feedboard";
+  }
 }
---
+++ sns-kitri/src/main/java/com/kitri/sns/feed/dao/FeedDAOImpl.java
@@ -0,0 +1,31 @@
+package com.kitri.sns.feed.dao;
+
+import com.kitri.sns.feed.vo.FeedVO;
+
+public class FeedDAOImpl implements FeedDAO {
+
+  @Override
+  public int selectFollowingNum() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public FeedVO selectFollowFeeds() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public FeedVO selectRandomFeeds() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public FeedVO selectMoreFeeds() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
